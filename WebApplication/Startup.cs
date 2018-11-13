2df6ef387921f915061b8000435be8ca496f0259
Vitaliy <vitaliy.lebedev7@gmail.com>
2018-11-13 15:21:07 +0600

    fix db connection

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -44,7 +44,7 @@ namespace WebApplication
             });
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
